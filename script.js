020055a83bd592c9459496b68679b05fbe7ac7a8
teeerapong2020 <teerapong.chue@gmail.com>
2024-05-13 14:38:56 +0700

    finish2

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ const milk ={
     product_name:'milkgood',
     product_id:2022,
     quantily:300,
-    unit_price:30
+    unit_price:30,
+    description:{
+        brand:'Bigmilk',
+        quantily:300,
+        taste:'good',
+        colr:'while',
+        additional:'very big'
+     }
 };
-const description ={
-   brand:'Bigmilk',
-   quantily:300,
-   taste:'good',
-   colr:'while',
-   additional:'very big'
-}
-console.log(milk,description)
+
+console.log(milk)
